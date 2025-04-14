d41abd845e86c33ed76bc8584427e2344f5c2a68
this.vey <this.vey@gmail.com>
2025-04-14 20:32:10 +0000

    working on first blog post

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,6 +12,7 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		prompt: z.string().optional(),
 	}),
 });
 
