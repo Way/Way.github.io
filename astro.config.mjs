05c5b23148f86e693b3efe2cc16fb82b36a9802a
this.vey <this.vey@gmail.com>
2025-04-15 05:39:17 +0000

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://alexvey.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://alexvey.com',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
