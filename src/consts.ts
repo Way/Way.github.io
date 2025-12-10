7efebef00d2d4162af5af09cfa932a49f0087b40
Alexander Vey <av@fino.digital>
2025-12-10 20:24:28 +0100

    fix: update SITE_DESCRIPTION and wording in about page to enhance consistency

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Alex Vey';
-export const SITE_DESCRIPTION = 'People. Product. Technology.';
+export const SITE_DESCRIPTION = 'People. Life. Technology.';
